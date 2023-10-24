5e2efc43d16c18f9576b391481bf764a6cbc1da1
Markus Spöri <markus.spoeri@ionos.com>
2023-10-24 14:16:25 +0300

    working web server

---
+++ api-test.sh
@@ -0,0 +1,8 @@
+#curl --location https://api.ionos.com/databases/quota \
+#  -H "Authorization: Bearer $IONOS_TOKEN" | jq
+#
+#curl --location https://dns.de-fra.ionos.com/quota \
+#  -H "Authorization: Bearer $IONOS_TOKEN" | jq
+
+
+curl -v localhost:8080/quotas | jq
