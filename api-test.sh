cc08648f269f87ea764b9f7c104a6a2a1700cc56
Markus Spöri <markus.spoeri@ionos.com>
2023-10-25 12:50:15 +0300

    clean slate

--- api-test.sh
+++
@@ -1,8 +0,0 @@
-#curl --location https://api.ionos.com/databases/quota \
-#  -H "Authorization: Bearer $IONOS_TOKEN" | jq
-#
-#curl --location https://dns.de-fra.ionos.com/quota \
-#  -H "Authorization: Bearer $IONOS_TOKEN" | jq
-
-
-curl -v localhost:8080/quotas | jq
